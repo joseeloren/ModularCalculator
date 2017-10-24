6b2cad56023441ccf8cc3d1814aff9e43c6fca64
Jose Lorenzo <isleo11@gmail.com>
2017-10-24 21:35:30 +0100

    Adding add

--- modularcalculator.R
+++ modularcalculator.R
@@ -1,4 +1,8 @@
-doc <- "This is a modular calculator. Choose an option: 1. Add 2. Subtract 3. Multiply 4. Divide"
-doc
+n <- readline(prompt="This is a modular calculator. Choose an option: 1. Add 2. Subtract 3. Multiply 4. Divide")
+n1 <- readline(promt="First num")
+n2 <- readline(promt="Second num")
+nI = as.integer(n)
 
-
+if (nI == 1) {
+  n1+n2;
+}
